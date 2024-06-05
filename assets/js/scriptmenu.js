f59562f56e6d5f7383289f6fa727e06634d691fa
Verymew <111842541+verymew@users.noreply.github.com>
2024-06-05 01:17:01 -0300

    .

--- assets/js/scriptmenu.js
+++
@@ -1,8 +0,0 @@
-function menuShow(){
-    let menuzinho = document.querySelector('.menu-mobile');
-    if(menuzinho.classList.contains('open')){
-        menuzinho.classList.remove('open');
-    }else {
-        menuzinho.classList.add('open');
-    }
-}
